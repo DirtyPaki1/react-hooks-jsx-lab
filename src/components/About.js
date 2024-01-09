2fba9dbb70c5e474d2355109866ed6b153bb086d
Usman <91799557+DirtyPaki1@users.noreply.github.com>
2024-01-08 23:07:43 -0500

    Completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { image } from "../data/data";
 
-function About() {
-  return <div>About</div>;
+const About =()=>{
+  return(
+    <div id="about">
+      <p> Hello</p>
+      <img src={{image}} alt="I made this"></img>
+    </div>
+  )
 }
 
+
 export default About;
+
